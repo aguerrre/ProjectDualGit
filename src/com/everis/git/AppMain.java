a5ce616cc4d21f71b483612688fd12f1f1749490
aguerrre <agustinguerrero.reyes@gmail.com>
2021-03-25 09:09:19 +0100

    Version 1

---
+++ src/com/everis/git/AppMain.java
@@ -0,0 +1,21 @@
+package com.everis.git;
+
+/**
+ * Formacion Git
+ * 
+ * @param args
+ */
+
+public class AppMain {
+	/**
+	 * Metodo main
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+
+	}
+
+}
